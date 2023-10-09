d75a41b3b05918b2dd58d41e625aaf30bc4e0570
racostab <racostab4@gmail.com>
2023-10-09 16:59:15 -0600

    Ejemplo Practica No. 1
    Ver contenido

---
+++ Practica_1/README.md
@@ -0,0 +1,8 @@
+# Practicas_cursos
+Este proyecto contiene múltiples ejemplos de programas sencillos para
+ser usados en los cursos:
+
+1. Análisis y Diseño de Algoritmos
+2. Análisis y Diseño de Algoritmos concurrentes y distribuidos
+3. Diseño de Sistemas Operatvos
+
---
+++ Practica_1/procesos_cpp_v1.cpp
@@ -0,0 +1,30 @@
+/*
+   C++ Program to demonstrate tthe use of Fork.
+   Instalacion:
+      $ sudo apt-get update
+	  $ sudo apt-get install build-essential 
+   Compilar
+      $ g++ -o procesos_cpp_v1 procesos_cpp_v1.cpp
+*/
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+	pid_t pid = fork();
+
+	switch(pid){
+	  case -1: // ERROR
+		       perror("fork");
+		       exit(EXIT_FAILURE);
+	  case 0: // HIJO
+	           cout << "Child  PID: " << getpid() << endl;
+				break;
+	  default: // PADRE
+		       cout << "Father PID: " << getpid() << endl;
+
+	}
+	return 0;
+}

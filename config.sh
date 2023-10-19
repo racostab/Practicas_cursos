6330c6f3bac163a1529a8db223411a54a05a1d5d
racostab <racostab4@gmail.com>
2023-10-19 16:17:27 -0600

    config.sh file
    Correccion

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ clean()
    for file in *
    do
       #echo $file
-      if [ -f $file -a -x $file -a $file != "config.sh" ]
+      if [ -f $file -a -x $file -a $file != "config.sh" -a "$file" == "${file/\./}" ]
       then
          echo "rm -f $file"
          rm -f $file

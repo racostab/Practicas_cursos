c065dff4e51de5c9afb560ed53c1109ec7256b8b
racostab <racostab4@gmail.com>
2023-10-23 19:12:34 -0600

    Pipes 1
    .

--- IPC_pipes/pipes_v2.c
+++ IPC_pipes/pipes_v2.c
@@ -7,18 +7,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 //int main (int argc, char **argv)
 int main()
 {
- int error, fd[2], pid, tam=5;
- char *buf="abcde";
+ int error, fd[2], pid, tam=5, bytes=0;
+ //char *buf="abcde";
+ char *buf;
 
    error = pipe(fd);
    if( error < 0 ){
      printf("Error en la creacion del pipe\n");
      return 1;
    }
+   //buf = malloc(50 * sizeof(char));
+   buf = calloc(50, sizeof(char));
 
    pid = fork();
    switch(pid){
@@ -26,10 +30,12 @@ int main()
                   printf("Error de creacion");
                   exit(1);
       case 0: // Hijo
-                  read(fd[0], buf, tam);
-                  printf("H= %s \n", buf);
+                  //bytes = read(fd[0], buf, tam);
+                  bytes = read(fd[0], buf, 50);
+                  printf("H= %s %d\n", buf, bytes);
                   break;
       default:
+                  strcpy(buf, "abcde");
                   sleep(4);
                   write(fd[1], buf, tam);
                   printf("P= %s \n", buf);
